1405304c6164bf7078215009740364044a44edfe
juanzan92 <juanzan92@gmail.com>
2017-08-09 18:55:00 -0300

    Update Program.cs

--- tp1/TP1/TP1/Program.cs
+++ tp1/TP1/TP1/Program.cs
@@ -13,7 +13,6 @@ namespace TP1
         
 
             public class CongruencialMixto
-
         {
             //constante a
             public int a { get; set; }
@@ -26,41 +25,109 @@ namespace TP1
 
 
 
-            int [] numAleatorios ;
+            List<int> numAleatorios = new List<int>();
 
 
 
 
             //valida si se puede generar segun las variables ingresadas
-            public  bool puedoGenerar()
+            public bool puedoGenerar()
             {
 
-                    return a != 0 && c != 0 && m != 0 && m > x0 && m > a && m > c;
-                
+                return a != 0 && c != 0 && m != 0 && m > x0 && m > a && m > c;
+
             }
 
             //genera 20 numero aleatorios s
             public void generar20()
-
             {
                 for (int i = 0; i < 20; i++)
                 {
-                  numAleatorios.SetValue ((  x0 = ((a * x0) + c) % m),i);
-                   
+                    Console.WriteLine("X₁ = (({0} ٠ {1}) + {3}) mod {2}", a, x0, m, c);
+                    numAleatorios.Add(x0 = ((a * x0) + c) % m);
+                    Console.WriteLine("X₁ = " + x0);
+                  
+
                 }
             }
             //generera el sig numero aleatrorio
-            public  int generarSiguiente()
+            public void generarSiguiente()
             {
-                return ((a * x0) + c) % m;
+                Console.WriteLine("X₁ = (({0} ٠ {1}) + {3}) mod {2}", a, x0, m, c);
+                numAleatorios.Add(x0 = ((a * x0) + c) % m);
+                Console.WriteLine("X₁ = " + x0);
+              
+
+
+
             }
 
 
-            public override string ToString()
+            public override void ToString()
             {
-                return string.Format("X₁ = (({0} ٠ {1}) + {3}) mod {2}", a, x0, m, c);
+                foreach (object x in numAleatorios)
+                {
+                    Console.WriteLine(x.ToString());
+                }
+
+
             }
+
+
         }
+
+        class CongruenteMultiplicativo
+        {
+            //constante a
+            public int a { get; set; }
+
+            //raiz x0
+            public int x0 { get; set; }
+            //constante m 
+            public int m { get; set; }
+
+
+
+            List<int> numAleatorios = new List<int>();
+
+
+            //valida si se puede generar segun las variables ingresadas
+            public bool puedoGenerar()
+            {
+
+                return a != 0 && m != 0 && m > x0 && m > a;
+
+            }
+
+            //genera 20 numero aleatorios s
+            public void generar20()
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.WriteLine("X₁ = ({0} ٠ {1} ) mod {3}", a, x0, m);
+                    numAleatorios.Add(x0 = ((a * x0) % m));
+                    Console.WriteLine("X₁ = " + x0);
+                  
+
+                }
+
+             }
+
+            public void generarSiguiente()
+            {
+                Console.WriteLine("X₁ = (({0} ٠ {1})  mod {3}", a, x0, m);
+                numAleatorios.Add(x0 = (a * x0) % m);
+                Console.WriteLine("X₁ = " + x0);
+
+
+
+            }
+
+
+
+
+        }
+
 
     
 
